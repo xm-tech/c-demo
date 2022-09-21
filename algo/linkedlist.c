6a38207e441805362de0dc2c5c7ce186a38b2198
xm-tech <maxm09@163.com>
2022-09-21 08:12:28 +0800

    continue on linkedlist

--- algo/linkedlist.c
+++ algo/linkedlist.c
@@ -10,23 +10,14 @@ typedef struct node {
 // 输出链表每个节点的值
 void display(node_t * head);
 
-// 在链表尾部添加1节点
-void push_end(node_t * head, int val);
+// 表头添加
+bool push(node_t * head, int val);
 
-// 在链表头部添加1节点
-void push_head(node_t * head, int val);
-
-// 在指定索引位置添加1节点
-void push_any(node_t * head, int val);
-
-// 删除表头的节点
+// 表尾删除
 int pop(node_t * head);
 
-// 删除表尾的节点
-int remove_end(node_t * head);
-
 // 删除1指定索引位置的节点
-int remove_index_node(node_t * head, int index);
+int remove_index(node_t * head, int index);
 
 // 查找是否包含某个值
 bool search(node_t * head, int val);
@@ -38,22 +29,25 @@ void display(node_t * head) {
 		printf("current.val=%d\n", current->val);
 		current = current->next;
 	}
+	free(current);
+}
+
+bool push(node_t * head, int val){
+	node_t * new = (node_t *)malloc(sizeof(node_t));
+	new->val = val;
+	new->next = head;
+	head = new;
+	return true;
 }
 
 int main(int argc, char **argv) {
 	node_t * head = (node_t *)malloc(sizeof(node_t));
-	if (head == NULL) {
-		printf("malloc fail 1");
-		return 1;
-	}
 	head->val=1;
 	head->next = (node_t *)malloc(sizeof(node_t));
-	if (head->next == NULL) {
-		printf("malloc fail 2");
-		return 2;
-	}
 	head->next->val = 2;
 	head->next->next = NULL;
+
+	push(head, 3);
 
 	display(head);
 	return 0;	
