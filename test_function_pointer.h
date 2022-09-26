bf7b2569a8b56a2cddf58f08d4212872b22650c6
xm-tech <maxm09@163.com>
2022-09-26 13:09:31 +0800

    comment

--- test_function_pointer.h
+++ test_function_pointer.h
@@ -1,3 +1,3 @@
 // add is a function pointer, which can be easiely used as a function parameter
-// func 是 1 指向函数的指针
+// func 是 1 指向函数的指针, 这里的参数和返回值都可以是任意结构体，那么就可以像 skynet 一样实现通用的函数回调
 typedef int(*func) (int, int);
