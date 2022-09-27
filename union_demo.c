eeee07ffc5584fa063d96c7fd61d862807a5c45e
xm-tech <maxm09@163.com>
2022-09-27 08:02:02 +0800

    about union

--- union_demo.c
+++ union_demo.c
@@ -12,10 +12,15 @@ typedef union {
 
 int main(){
 	data_u_t* d_u = malloc(sizeof(data_u_t));
-	d_u->f = 3.3;
+	d_u->f = 3.3f;
 	printf("d_u.f=%.2f\n", d_u->f);
 
-	assert(d_u->i = 33);
+	assert(d_u->f == 3.3f);
+	/* assert(d_u->i = 33); */
+
+	// expect abort
+	assert(d_u->i = 0);
+
 	printf("d_u.i=%d\n", d_u->i);
 	printf("d_u.f=%.2f\n", d_u->f);
 }
