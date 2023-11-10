5607884b61d044b018654e819337f6e90edbb64f
xm-tech <maxm09@163.com>
2023-11-10 19:56:45 +0800

    pthread demo

--- pthread/pthread_demo.c
+++ pthread/pthread_demo.c
@@ -3,6 +3,11 @@
 #include <unistd.h>
 #include <pthread.h>
 
+typedef struct person {
+	char *name;
+	int age;
+} person;
+
 typedef struct req {
 	int cmd;
 	void * data;
@@ -22,6 +27,13 @@ resp *handle(req *r) {
 	return res;
 }
 
+person* create_person(char *name, int age) {
+	person *p = malloc(sizeof(person));
+	p->name = name;
+	p->age = age;
+	return p;
+}
+
 void *doSth(void *p) {
 	resp *res = handle((req *)p);
 	pthread_t cur_t = pthread_self();
@@ -29,7 +41,21 @@ void *doSth(void *p) {
 	return NULL;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+	printf("argc:%d\n", argc);
+	for (int i=0; i < argc; i++) {
+		printf("argv[%d]:%s\n", i, argv[i]);
+	}
+
+	char *name = argv[1];
+	// char *age = argv[2];
+	int age = atoi(argv[2]);
+	printf("name:%s,age:%d\n", name, age);
+	person *p = create_person(name, age);
+	char ret[100];
+	sprintf(ret, "create_person succ,name=%s,age=%d", p->name, p->age);
+	printf("%s\n", ret);
+
 	printf("before pthread create\n");
 
 	req *data = malloc(sizeof(*data));
